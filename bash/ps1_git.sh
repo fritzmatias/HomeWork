9069fbcc4ddcabb0b9a7b2512e159dad2f07dc3c
Matias D. Frtiz <fritzmatias@gmail.com>
2017-11-11 23:09:56 -0300

    create cache

--- bash/ps1_git.sh
+++ bash/ps1_git.sh
@@ -9,13 +9,18 @@
 ## 	execute the script inside the ~/.bashrc with: '. scriptName'
 ##
 # $(git status -s | grep ?? 2>/dev/null | wc -l)
+# git rev-parse --show-toplevel
+#
+#
+#
 ## check if some color is set
 if echo "$PS1" | grep '\\\[\\033\[' >/dev/null 2>&1 ; then
 #       PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$'
         PS1="${PS1}"\
-"\$(git branch >/dev/null 2>&1 && echo '\[\033[01;30m\]git:'\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\
-\$(if [ \$(git status -s 2>/dev/null | wc -l) -gt 0 ];then\
-         echo '\[\033[01;31m\]:unsync(AMD:'\$(git status -s | egrep '^[ AMD]{2,2}' 2>/dev/null | wc -l)',?:'\$(git status -s | egrep '^\?\?' 2>/dev/null | wc -l)')';\
+"\$(git branch >/dev/null 2>&1 && echo '\[\033[01;30m\]git: '\$(git branch 2>/dev/null | grep '^*' | colrm 1 2 &&\
+  cachefile=\$(git rev-parse --show-toplevel)/.git.cache &&\
+  if [ \$(! [ -f \"\${cachefile}\" ] && git status -s >\"\${cachefile}\" 2>/dev/null ; cat \"\${cachefile}\" 2>/dev/null | wc -l ) -gt 0 ];then\
+         echo '\[\033[01;31m\]:unsync(AMD:'\$(egrep '^[ AMD]{2,2}' \${cachefile} 2>/dev/null | wc -l)',?:'\$(egrep '^\?\?' \${cachefile} 2>/dev/null | wc -l)')';\
 else echo '';fi)'\[\033[00m\] \$')";
 
 else
