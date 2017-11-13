e02d666747330f4288ff50672439f8bbb92d44ca
Matias D. Frtiz <fritzmatias@gmail.com>
2017-11-13 00:32:36 -0300

    showing the remote origin reference

--- bash/ps1_git.sh
+++ bash/ps1_git.sh
@@ -123,7 +123,7 @@ export GITCACHEENABLE=true;
 if echo "$PS1" | grep '\\\[\\033\[' >/dev/null 2>&1 ; then
 #       PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$'
         PS1="${PS1}"\
-"\$( [ "$GITCACHEENABLE"x == "true"x ] && branch=\$(git branch 2>/dev/null) && echo '\[\033[01;30m\]git: '\$(echo \"\${branch}\"| grep '^*' | colrm 1 2 &&\
+"\$( [ "$GITCACHEENABLE"x == "true"x ] && branch=\$(git branch 2>/dev/null) && echo '\[\033[01;30m\]git: '\$(remote=\$(git remote get-url origin 2>/dev/null | sed -e 's/.*:\/\/\|www.\|\.\(com\|org\|gov\|edu\).*//g'); echo \${remote} | egrep '^/' >/dev/null 2>&1 && echo local || echo \${remote} )' : '\$(echo \"\${branch}\"| grep '^*' | colrm 1 2 &&\
   cachefile=\$(gitCache) &&\
   if [ \$(! [ -f \"\${cachefile}\" ] && buildCache \"\${cachefile}\" ; cat \"\${cachefile}\" 2>/dev/null | wc -l ) -gt 0 ];then\
          echo '\[\033[01;31m\]:unsync(AMD:'\$(egrep '^[ AMD]{2,2}' \${cachefile} 2>/dev/null | wc -l)',?:'\$(egrep '^\?\?' \${cachefile} 2>/dev/null | wc -l)')';\
@@ -131,9 +131,12 @@ else echo '';fi)'\[\033[01;30m\] \$\[\033[00m\] ')";
 
 else
 #       PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$'
-        PS1="${PS1}""\$(git branch >/dev/null 2>&1 && echo 'git:'\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\
-\$(if [ \$(git status -s 2>/dev/null | wc -l) -gt 0 ];then echo ':unsync(AMD:'\$(git status -s | egrep '^[ AMD]{2,2}' 2>/dev/null | wc -l)',?:'\$(git status -s | egrep '^\?\?' 2>/dev/null | wc -l)')'; \
-else echo ''; fi)' \$')";
+        PS1="${PS1}"\
+"\$( [ "$GITCACHEENABLE"x == "true"x ] && branch=\$(git branch 2>/dev/null) && echo 'git: '\$(remote=\$(git remote get-url origin 2>/dev/null | sed -e 's/.*:\/\/\|www.\|\.\(com\|org\|gov\|edu\).*//g'); echo \${remote} | egrep '^/' >/dev/null 2>&1 && echo local || echo \${remote} )' : '\$(echo \"\${branch}\"| grep '^*' | colrm 1 2 &&\
+  cachefile=\$(gitCache) &&\
+  if [ \$(! [ -f \"\${cachefile}\" ] && buildCache \"\${cachefile}\" ; cat \"\${cachefile}\" 2>/dev/null | wc -l ) -gt 0 ];then\
+         echo ':unsync(AMD:'\$(egrep '^[ AMD]{2,2}' \${cachefile} 2>/dev/null | wc -l)',?:'\$(egrep '^\?\?' \${cachefile} 2>/dev/null | wc -l)')';\
+else echo '';fi)' \$ ')";
 
 fi
 PS1="${PS1}"' '
